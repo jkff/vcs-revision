955dcf9f0b6f2cba4f50aa93326028ea287a55cc
Eugene Kirpichov <ekirpichov@gmail.com>
2014-05-16 08:43:59 -0700

    Fixed crash in case the version control command did not exist

--- Distribution/VcsRevision/Mercurial.hs
+++ Distribution/VcsRevision/Mercurial.hs
@@ -1,13 +1,19 @@
 module Distribution.VcsRevision.Mercurial ( getRevision ) where
 
+import Control.Exception
 import System.Process
 import System.Exit
+
+tryIO :: IO a -> IO (Either IOException a)
+tryIO = try
 
 -- | Nothing if we're not in a mercurial repo, Just (hash,modified) if we're in a repo.
 getRevision :: IO (Maybe (String, Bool))
 getRevision = do
-    (exit,out,_) <- readProcessWithExitCode "hg" ["identify", "-i"] ""
-    case (exit,init out,last (init out)) of
-        (ExitSuccess,hash,'+') -> return $ Just (init hash, True)
-        (ExitSuccess,hash,_)   -> return $ Just (hash, False)
-        (ExitFailure _,_,_)    -> return Nothing
+  res <- tryIO $ readProcessWithExitCode "hg" ["identify", "-i"] ""
+  case res of
+    Left ex -> return Nothing
+    Right (exit,out,_) -> case (exit,init out,last (init out)) of
+      (ExitSuccess,hash,'+') -> return $ Just (init hash, True)
+      (ExitSuccess,hash,_)   -> return $ Just (hash, False)
+      (ExitFailure _,_,_)    -> return Nothing
--- Distribution/VcsRevision/Svn.hs
+++ Distribution/VcsRevision/Svn.hs
@@ -1,17 +1,23 @@
 module Distribution.VcsRevision.Svn ( getRevision ) where
 
+import Control.Exception
 import System.Process
 import System.Exit
 import Data.List
 
+tryIO :: IO a -> IO (Either IOException a)
+tryIO = try
+
 -- | Nothing if we're not in a svn repo, Just (revision,modified) if we're in a repo.
 getRevision :: IO (Maybe (String, Bool))
 getRevision = do
-    (exit,info,_) <- readProcessWithExitCode "svn" ["info"] ""
-    case exit of
-        ExitSuccess -> do
-          let prefix = "Last Changed Rev: "
-          let rev = drop (length prefix) $ head $ filter (prefix `isPrefixOf`) (lines info)
-          (_,out,_) <- readProcessWithExitCode "svn" ["st", "-q"] ""
-          return $ Just (rev, out /= "")
-        _ -> return Nothing
+  res <- tryIO $  readProcessWithExitCode "svn" ["info"] ""
+  case res of
+    Left ex -> return Nothing
+    Right (exit,info,_) -> case exit of
+      ExitSuccess -> do
+        let prefix = "Last Changed Rev: "
+        let rev = drop (length prefix) $ head $ filter (prefix `isPrefixOf`) (lines info)
+        (_,out,_) <- readProcessWithExitCode "svn" ["st", "-q"] ""
+        return $ Just (rev, out /= "")
+      _ -> return Nothing
